1fe1fdc1277800d0691fd2bb289950f70c306295
Ahmed Kadhim <a.k.kadhim@outlook.com>
2024-06-17 10:05:18 +0200

    add missing function

--- DirectoriesUtil.py
+++ DirectoriesUtil.py
@@ -18,6 +18,10 @@ class Dicrectories:
     
     @staticmethod
     def pickle_by_id(path, id):
-        return os.path.join(path , str(id) + '.pkl')
-
+        return os.path.join(path , str(id) + '.pkl')
+
+    @staticmethod
+    def pickle_exist(path):
+        return os.path.exists(path)
+
 
